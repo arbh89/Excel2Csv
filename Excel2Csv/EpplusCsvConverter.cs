bad9ad5ac80f70f148eb575e089d85abddd74a95
Angel Blandon <arbh89@gmail.com>
2017-02-07 20:01:23 -0600

    Add project files.

---
+++ Excel2Csv/EpplusCsvConverter.cs
@@ -0,0 +1,127 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Excel2Csv
+{
+    public static class EpplusCsvConverter
+    {
+        public static byte[] ConvertToCsv(this ExcelPackage package)
+        {
+            var worksheet = package.Workbook.Worksheets[1];
+
+            var maxColumnNumber = worksheet.Dimension.End.Column;
+            var currentRow = new List<string>(maxColumnNumber);
+            var totalRowCount = worksheet.Dimension.End.Row;
+            var currentRowNum = 1;
+
+            var memory = new MemoryStream();
+
+            using (var writer = new StreamWriter(memory, Encoding.ASCII))
+            {
+                while (currentRowNum <= totalRowCount)
+                {
+                    BuildRow(worksheet, currentRow, currentRowNum, maxColumnNumber);
+                    WriteRecordToFile(currentRow, writer, currentRowNum, totalRowCount);
+                    currentRow.Clear();
+                    currentRowNum++;
+                }
+            }
+
+            return memory.ToArray();
+        }
+
+        public static byte[] ConvertToCsv(this ExcelWorksheet worksheet, string filePath = "")
+        {
+            var maxColumnNumber = worksheet.Dimension.End.Column;
+            var currentRow = new List<string>(maxColumnNumber);
+            var totalRowCount = worksheet.Dimension.End.Row;
+            var currentRowNum = 1;
+
+            var memory = new MemoryStream();
+
+            using (var writer = new StreamWriter(memory, Encoding.ASCII))
+            {
+                while (currentRowNum <= totalRowCount)
+                {
+                    BuildRow(worksheet, currentRow, currentRowNum, maxColumnNumber);
+                    WriteRecordToFile(currentRow, writer, currentRowNum, totalRowCount, worksheet.Name, filePath);
+                    currentRow.Clear();
+                    currentRowNum++;
+                }
+            }
+
+            return memory.ToArray();
+        }
+
+        public static bool IsWorksheetEmpty(this ExcelWorksheet worksheet)
+        {
+            return worksheet.Dimension == null;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="record">List of cell values</param>
+        /// <param name="sw">Open Writer to file</param>
+        /// <param name="rowNumber">Current row num</param>
+        /// <param name="totalRowCount"></param>
+        /// <remarks>Avoiding writing final empty line so bulk import processes can work.</remarks>
+        private static void WriteRecordToFile(List<string> record, StreamWriter sw, int rowNumber, int totalRowCount, string worksheetName = "", string filePath = "")
+        {
+            if (!string.IsNullOrEmpty(worksheetName) && !string.IsNullOrEmpty(filePath))
+            {
+                record.Insert(0, worksheetName);
+            }
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                record.Insert(0, filePath);
+            }
+
+            var commaDelimitedRecord = record.ToDelimitedString(",");
+
+            if (rowNumber == totalRowCount)
+            {
+                sw.Write(commaDelimitedRecord);
+            }
+            else
+            {
+                sw.WriteLine(commaDelimitedRecord);
+            }
+        }
+
+        private static void BuildRow(ExcelWorksheet worksheet, List<string> currentRow, int currentRowNum, int maxColumnNumber)
+        {
+            for (int i = 1; i <= maxColumnNumber; i++)
+            {
+                var cell = worksheet.Cells[currentRowNum, i];
+                if (cell == null)
+                {
+                    // add a cell value for empty cells to keep data aligned.
+                    AddCellValue(string.Empty, currentRow);
+                }
+                else
+                {
+                    AddCellValue(GetCellText(cell), currentRow);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Can't use .Text: http://epplus.codeplex.com/discussions/349696
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        private static string GetCellText(ExcelRangeBase cell)
+        {
+            return cell.Value == null ? string.Empty : cell.Value.ToString();
+        }
+
+        private static void AddCellValue(string s, List<string> record)
+        {
+            record.Add(string.Format("{0}{1}{0}", '"', s));
+        }
+    }
+}
---
+++ Excel2Csv/Program.cs
@@ -0,0 +1,141 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Excel2Csv
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("The syntax of the command is incorrect, type --help for more information");
+            }
+            else
+            {
+                if (HelpRequired(args[0]))
+                {
+                    ShowHelp();
+                }
+                else if (args.Length >= 2)
+                {
+                    if (IsValidPath(args[0]))
+                    {
+                        var folderPath = args[0];
+                        var outputPath = args[1];
+                        int fileCount = 1;
+                        var filesToConvet = new List<string>();
+                        var addExtraData = false;
+                        //var fileFilterPattern = "*.xlsx";
+
+                        if (File.Exists(folderPath))
+                        {
+                            filesToConvet.Add(folderPath);
+                        }
+                        else
+                        {
+                            filesToConvet.AddRange(Directory.EnumerateFiles(folderPath, "*.xlsx"));
+                            filesToConvet.AddRange(Directory.EnumerateFiles(folderPath, "*.xlsm"));
+                        }
+
+                        //check if extra data will be added
+                        if (args.Length >= 3)
+                        {
+                            if (args[2] == "/E")
+                            {
+                                addExtraData = true;
+                            }
+                            //else if (args[2] == "/P")
+                            //{
+                            //    fileFilterPattern = args[2];
+                            //}
+                            //else if (args.Length == 4)
+                            //{
+                            //    if (args[3] == "/E")
+                            //    {
+                            //        addExtraData = true;
+                            //    }
+                            //}
+                            else
+                            {
+                                Console.WriteLine("The syntax of the command is incorrect, type --help for more information");
+                                return;
+                            }
+                        }
+
+                        //If output directory does not exists it gets created
+                        if (!Directory.Exists(outputPath))
+                        {
+                            FileInfo file = new System.IO.FileInfo(outputPath);
+                            file.Directory.Create();
+                        }
+
+                        foreach (string file in filesToConvet)
+                        {
+                            var excelFile = new FileInfo(file);
+                            using (var package = new ExcelPackage(excelFile))
+                            {
+                                foreach (var item in package.Workbook.Worksheets)
+                                {
+                                    if (!item.IsWorksheetEmpty())
+                                    {
+                                        byte[] csvFile;
+                                        if (addExtraData)
+                                        {
+                                            csvFile = item.ConvertToCsv(file);
+                                        }
+                                        else
+                                        {
+                                            csvFile = item.ConvertToCsv();
+                                        }
+
+                                        File.WriteAllBytes(outputPath + "\\" + fileCount + " - " + item.Name + ".csv", csvFile);
+                                        fileCount++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The syntax of the command is incorrect, type --help for more information");
+                }
+            }
+        }
+
+        private static bool IsValidPath(string path)
+        {
+            var isValid = true;
+            try
+            {
+                var fullPath = System.IO.Path.GetFullPath(path);
+            }
+            catch (Exception)
+            {
+                isValid = false;
+            }
+            return isValid && (System.IO.Directory.Exists(path) || System.IO.File.Exists(path));
+        }
+
+        private static bool HelpRequired(string param)
+        {
+            return param == "-h" || param == "--help" || param == "/?";
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine(@"
+Converts every worksheet from excel files (.xlsx) to csv
+
+excel2csv source destination
+
+source       Specifies the file or folder to be converted.
+destination  Specifies the directory for the new file(s). If not exists them is created
+filter       Indicates the patter to select files in folder (*.xlsx)
+/E           Indicates if extra data like file name and sheet name will be added to the output file");
+        }
+    }
+}
